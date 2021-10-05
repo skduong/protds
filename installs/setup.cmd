edf8e6a0a5026ae21c894bf734d5bdc1154a24f0
skduong <skduong@outlook.com>
2021-10-05 10:10:14 -0500

    fixed indentation issues

--- installs/setup.cmd
+++ installs/setup.cmd
@@ -1,10 +1,9 @@
-@ECHO OFF
-call C:\ProgramData\Anaconda3\Scripts\activate.bat
+@echo off
+call C:\Anaconda3\Scripts\activate.bat
 pip install -r requirements.txt
 ::icn3dpy reqs
 conda install -c conda-forge nodejs
 jupyter labextension install jupyterlab_3dmol
-::ipynb shortcuts
-python3 -m pip install nbopen
-python3 -m nbopen.install_win
-::C:\Users\SUL\anaconda3
+::pdb query
+pip install git
+pip install git+git://github.com/williamgilpin/pypdb
